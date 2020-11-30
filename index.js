ac10ea00f32c281faa98417e83ce4130099d2cdf
VEDANSH DHARWAL <vedansh.dharwal@gmail.com>
2020-11-30 16:08:54 +0530

    Heroku Changes

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.SECRET_KEY);
-
-//new method of checkout
-// const stripe = require("stripe")(
-//   "SECRET_KEY"
-// );
-
+const path = require("path");
 const { uuid } = require("uuidv4");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 //middleware
 
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(cors());
 
@@ -27,7 +22,7 @@ app.post("/payment", (req, res) => {
   //token will contain product details - price etc
   // console.log(req.body);
   const { item, token } = req.body;
-  console.log("token: ", token);
+  // console.log("token: ", token);
   // console.log("Price: ", item.price);
 
   // to avoid double charging the same item
@@ -64,29 +59,15 @@ app.post("/payment", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-// app.post("/payment", async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ["card"],
-//     line_items: [
-//       {
-//         price_data: {
-//           currency: "usd",
-//           product_data: {
-//             name: "T-shirt",
-//           },
-//           unit_amount: 2000,
-//         },
-//         quantity: 1,
-//       },
-//     ],
-//     mode: "payment",
-//     success_url: "https://example.com/success",
-//     cancel_url: "https://example.com/cancel",
-//   });
+// serve static files if we are in production mode
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(client / build));
 
-//   res.json({ id: session.id });
-// });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-//listen
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`LISTENING AT PORT ${PORT}`));
